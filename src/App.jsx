78e11c06ee1b538acffa7612717dcd9843394dff
ankitjhajhariya <ankitjhajhariya018@gmail.com>
2025-06-03 17:26:06 +0530

    Your message

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import Button from './compnent/button'
 import './App.css'
-import Nav from './Nav'
+import Nav from './compnent/Nav'
 import Hero from './compnent/hero'
-import Aboutus from './aboutus'
+import Aboutus from './compnent/aboutus'
+import '@fontsource/poppins'; 
+
 
 function App() {
 
